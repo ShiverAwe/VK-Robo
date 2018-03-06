9d736e899b7bd2c21c1ff5901505fc08fd646038
unknown <quameu@gmail.com>
2018-03-06 20:26:34 +0300

    Initial

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,123 +1,74 @@
+import com.vk.api.sdk.client.VkApiClient
+import com.vk.api.sdk.client.actors.UserActor
+import com.vk.api.sdk.httpclient.HttpTransportClient
+import com.vk.api.sdk.objects.users.UserXtrCounters
+import com.vk.api.sdk.queries.users.UserField
 import java.awt.Desktop
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.net.URL
-import java.nio.charset.StandardCharsets
-import java.util.*
+import java.util.HashMap
 
+class Application {
+    val APP_ID = 6383627
+    val CLIENT_SECRET = "ZbOMwURj5cZ6P612exgc"
+    //val code = "644ef1146078f41b37"
+    val EXPIRES_IN = "86400"
+    val USER_ID = 135383817
+    val API_VERSION = "5.21"
+    val DISPLAY = "page"
+    val REDIRECT_URI = "https://oauth.vk.com/blank.html"
+    val PERMISSIONS = "photos,messages"
+    val RESPONSE_TYPE = "code"
 
-fun with(appId: String, accessToken: String): Application {
-    return Application(appId, accessToken)
-}
+    fun main(code: String) {
+        val transportClient = HttpTransportClient.getInstance()
+        val vk = VkApiClient(transportClient)
+        val authResponse = vk.oauth()
+                .userAuthorizationCodeFlow(APP_ID, CLIENT_SECRET, REDIRECT_URI, code)
+                .execute()
 
-class Application(
-        private val appId: String = "",
-        private val accessToken: String = "dfd4356905a55263116b891f55f74ba8f40fbf6b74c5b292bbd7e1768279860ba12560f2c835143b02a7"
-) {
+        val actor = UserActor(authResponse.userId, authResponse.accessToken)
 
-    private val APP_ID = appId
-    private val EXPIRES_IN = "86400"
-    private val USER_ID = "135383817"
-    private val API_VERSION = "5.21"
-    private val DISPLAY = "page"
-    private val REDIRECT_URI = "https://oauth.vk.com/blank.html"
-    private val PERMISSIONS = "photos,messages"
+        val userok: MutableList<UserXtrCounters> = vk.users().get(actor)
+                .userIds("19383739")
+                .fields(UserField.ABOUT,
+                        UserField.CITY,
+                        UserField.LAST_SEEN)
+                .execute()
 
-    init {
-        if (accessToken.isEmpty()) {
-            auth(appId)
-            throw Error("Need access token")
+        println(userok)
+
+//        val getResponse = vk.wall().get(actor)
+//                .ownerId(USER_ID)
+//                .count(100)
+//                .offset(5)
+////            .filter("owner")
+//                .execute()
+//
+//        println(getResponse)
+    }
+
+    fun auth() {
+        val AUTH_URL = ("https://oauth.vk.com/authorize"
+                + "?client_id=${APP_ID}"
+                + "&scope=${PERMISSIONS}"
+                + "&redirect_uri=${REDIRECT_URI}"
+                + "&display=${DISPLAY}"
+                + "&v=${API_VERSION}"
+                + "&response_type=${RESPONSE_TYPE}")
+
+        Desktop.getDesktop().browse(URL(AUTH_URL).toURI())
+    }
+
+    fun getProperties(query: String): Map<String, String> {
+        val params: List<String> = query.trim().split("[&#]".toRegex())
+        println(params)
+        val map: HashMap<String, String> = HashMap()
+        for ((index, param) in params.withIndex()) {
+            if (index == 0) continue
+            val name = param.split("=")[0]
+            val value = param.split("=")[1]
+            map[name] = value
         }
-    }
-
-    private val AUTH_URL = ("https://oauth.vk.com/authorize"
-            + "?client_id=${APP_ID}"
-            + "&scope=${PERMISSIONS}"
-            + "&redirect_uri=${REDIRECT_URI}"
-            + "&display=${DISPLAY}"
-            + "&v=${API_VERSION}"
-            + "&response_type=token")
-
-    private val API_REQUEST = ("https://api.vk.com/method/{METHOD_NAME}"
-            + "?{PARAMETERS}"
-            + "&access_token={ACCESS_TOKEN}"
-            + "&v=" + API_VERSION)
-
-    private fun auth(appId: String, scope: String, redirectUri: String) = ("https://oauth.vk.com/authorize"
-            + "?client_id=${appId}"
-            + "&scope=${scope}"
-            + "&redirect_uri=${redirectUri}"
-            + "&display=${DISPLAY}"
-            + "&v=${API_VERSION}"
-            + "&response_type=token")
-
-    private fun auth(appId: String) {
-        val reqUrl = AUTH_URL
-        Desktop.getDesktop().browse(URL(reqUrl).toURI())
-    }
-
-    fun getDialogs(): String {
-        return invokeApi("messages.getDialogs", null)
-    }
-
-    fun getHistory(userId: String, offset: Int, count: Int, rev: Boolean): String {
-        return invokeApi("messages.getHistory", Params.create()
-                .add("user_id", userId)
-                .add("offset", offset.toString())
-                .add("count", count.toString())
-                .add("rev", if (rev) "1" else "0"))
-    }
-
-    fun getAlbums(userId: String): String {
-        return invokeApi("photos.getAlbums", Params.create()
-                .add("owner_id", userId)
-                .add("photo_sizes", "1")
-                .add("thumb_src", "1"))
-    }
-
-    private fun invokeApi(method: String, params: Params?): String {
-        val parameters = params?.build() ?: ""
-        val reqUrl = API_REQUEST
-                .replace("{METHOD_NAME}", method)
-                .replace("{ACCESS_TOKEN}", accessToken)
-                .replace("{PARAMETERS}&", parameters)
-        return invokeApi(reqUrl)
-    }
-
-    private fun invokeApi(requestUrl: String): String {
-        val result = StringBuilder()
-        val url = URL(requestUrl)
-        url.openStream().use { `is` ->
-            val reader = BufferedReader(InputStreamReader(`is`, StandardCharsets.UTF_8))
-            reader.lines().forEach {
-                result.append(it)
-            }
-        }
-        return result.toString()
-    }
-
-    private class Params private constructor() {
-
-        private val params: HashMap<String, String> = HashMap()
-
-        fun add(key: String, value: String): Params {
-            params.put(key, value)
-            return this
-        }
-
-        fun build(): String {
-            if (params.isEmpty()) return ""
-            val result = StringBuilder()
-            params.keys.stream().forEach { key ->
-                result.append("${key}=${params[key]}&");
-            }
-            return result.toString()
-        }
-
-        companion object {
-            fun create(): Params {
-                return Params()
-            }
-        }
+        return map
     }
 }
---
+++ src/main/kotlin/ApplicationTest.kt
@@ -0,0 +1,123 @@
+import java.awt.Desktop
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.net.URL
+import java.nio.charset.StandardCharsets
+import java.util.*
+
+
+fun with(appId: String, accessToken: String): ApplicationTest {
+    return ApplicationTest(appId, accessToken)
+}
+
+class ApplicationTest(
+        private val appId: String = "",
+        private val accessToken: String = "dfd4356905a55263116b891f55f74ba8f40fbf6b74c5b292bbd7e1768279860ba12560f2c835143b02a7"
+) {
+
+    private val APP_ID = appId
+    private val EXPIRES_IN = "86400"
+    private val USER_ID = "135383817"
+    private val API_VERSION = "5.21"
+    private val DISPLAY = "page"
+    private val REDIRECT_URI = "https://oauth.vk.com/blank.html"
+    private val PERMISSIONS = "photos,messages"
+
+    init {
+        if (accessToken.isEmpty()) {
+            auth(appId)
+            throw Error("Need access token")
+        }
+    }
+
+    private val AUTH_URL = ("https://oauth.vk.com/authorize"
+            + "?client_id=${APP_ID}"
+            + "&scope=${PERMISSIONS}"
+            + "&redirect_uri=${REDIRECT_URI}"
+            + "&display=${DISPLAY}"
+            + "&v=${API_VERSION}"
+            + "&response_type=token")
+
+    private val API_REQUEST = ("https://api.vk.com/method/{METHOD_NAME}"
+            + "?{PARAMETERS}"
+            + "&access_token={ACCESS_TOKEN}"
+            + "&v=" + API_VERSION)
+
+    private fun auth(appId: String, scope: String, redirectUri: String) = ("https://oauth.vk.com/authorize"
+            + "?client_id=${appId}"
+            + "&scope=${scope}"
+            + "&redirect_uri=${redirectUri}"
+            + "&display=${DISPLAY}"
+            + "&v=${API_VERSION}"
+            + "&response_type=token")
+
+    private fun auth(appId: String) {
+        val reqUrl = AUTH_URL
+        Desktop.getDesktop().browse(URL(reqUrl).toURI())
+    }
+
+    fun getDialogs(): String {
+        return invokeApi("messages.getDialogs", null)
+    }
+
+    fun getHistory(userId: String, offset: Int, count: Int, rev: Boolean): String {
+        return invokeApi("messages.getHistory", Params.create()
+                .add("user_id", userId)
+                .add("offset", offset.toString())
+                .add("count", count.toString())
+                .add("rev", if (rev) "1" else "0"))
+    }
+
+    fun getAlbums(userId: String): String {
+        return invokeApi("photos.getAlbums", Params.create()
+                .add("owner_id", userId)
+                .add("photo_sizes", "1")
+                .add("thumb_src", "1"))
+    }
+
+    private fun invokeApi(method: String, params: Params?): String {
+        val parameters = params?.build() ?: ""
+        val reqUrl = API_REQUEST
+                .replace("{METHOD_NAME}", method)
+                .replace("{ACCESS_TOKEN}", accessToken)
+                .replace("{PARAMETERS}&", parameters)
+        return invokeApi(reqUrl)
+    }
+
+    private fun invokeApi(requestUrl: String): String {
+        val result = StringBuilder()
+        val url = URL(requestUrl)
+        url.openStream().use { `is` ->
+            val reader = BufferedReader(InputStreamReader(`is`, StandardCharsets.UTF_8))
+            reader.lines().forEach {
+                result.append(it)
+            }
+        }
+        return result.toString()
+    }
+
+    private class Params private constructor() {
+
+        private val params: HashMap<String, String> = HashMap()
+
+        fun add(key: String, value: String): Params {
+            params.put(key, value)
+            return this
+        }
+
+        fun build(): String {
+            if (params.isEmpty()) return ""
+            val result = StringBuilder()
+            params.keys.stream().forEach { key ->
+                result.append("${key}=${params[key]}&");
+            }
+            return result.toString()
+        }
+
+        companion object {
+            fun create(): Params {
+                return Params()
+            }
+        }
+    }
+}
