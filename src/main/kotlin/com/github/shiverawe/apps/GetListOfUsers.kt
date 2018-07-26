03df453adc6eda30fe37c93f669d8d4211cc01c4
ShiverAwe <quameu@gmail.com>
2018-07-26 20:25:45 +0300

    Minor cleanup

--- src/main/kotlin/com/github/shiverawe/apps/GetListOfUsers.kt
+++ src/main/kotlin/com/github/shiverawe/apps/GetListOfUsers.kt
@@ -2,14 +2,11 @@ package com.github.shiverawe.apps
 
 import com.github.shiverawe.temp.AuthData
 import com.github.shiverawe.temp.Requests
-import com.github.shiverawe.temp.UserIds
 
 
 fun main(args: Array<String>) {
     val code = AuthData.getCode()
     val actor = AuthData.getActor(code)
-    val users: IntArray = Requests.getFriendsOfUser(actor, UserIds.asMap["eneustroeva"]!!)
-//    val users: IntArray = Requests.getMembersOfGroup(actor, "76477009")
     val result = Requests.vk
             .groups()
             .getMembers(actor)
