b4fa571d223bef58b9f964d40478d7422342a5c3
ShiverAwe <quameu@gmail.com>
2018-08-19 22:39:27 +0300

    Create AddFriends app.

---
+++ src/main/java/com/github/shiverawe/vk/apps/AddFriendApp.kt
@@ -0,0 +1,57 @@
+package com.github.shiverawe.vk.apps
+
+import com.github.shiverawe.vk.temp.AuthData
+import com.github.shiverawe.vk.temp.Requests
+import com.vk.api.sdk.objects.friends.FriendStatusFriendStatus.*
+
+fun main(args: Array<String>) {
+    fun requiredInput(): String = readLine().orEmpty()
+    val code = requiredInput()//AuthData.getCode()
+    val actor = AuthData.getActor(code)
+    val usersTarget: List<Int> = requiredInput().toInts()
+    val usersExcluded: List<Int> = requiredInput().toInts()
+    val usersActive: List<Int> = usersTarget.subtract(usersExcluded).toList().reversed()
+    val usersSuccess: MutableList<Int> = ArrayList()
+
+
+    var TTL = 30
+    try {
+        usersActive
+                .forEach { userId ->
+                    val friendStatus = Requests.vk
+                            .friends()
+                            .areFriends(actor, userId)
+                            .execute()
+                            .get(0)
+                            .friendStatus
+                    when (friendStatus) {
+                        NOT_A_FRIEND, INCOMING_REQUEST -> {
+                            Requests.vk
+                                    .friends()
+                                    .add(actor, userId)
+                                    .execute()
+                            println("ID $userId : Adding...")
+                            TTL--
+                            usersSuccess.add(userId)
+                            Thread.sleep(5000)
+                        }
+                        IS_FRIEND -> println("ID $userId : Already friends")
+                        OUTCOMING_REQUEST -> println("ID $userId : Outcoming friends")
+                    }
+                    Thread.sleep(400)
+                }
+        if (TTL <= 0) throw RuntimeException("TTL ENDED")
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+    println(usersSuccess)
+    println("TTL $TTL")
+
+}
+
+
+fun String.toInts(): List<Int> = this
+        .split(",")
+        .map { it.trim() }
+        .filter { !it.isBlank() }
+        .map { it.toInt() }
