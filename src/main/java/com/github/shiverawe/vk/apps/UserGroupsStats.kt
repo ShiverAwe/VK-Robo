a6b9a5b8603d81f7f9179ab284f6b9ab91a9dcae
ShiverAwe <quameu@gmail.com>
2018-08-20 21:40:20 +0300

    Refactor

--- src/main/java/com/github/shiverawe/vk/apps/UserGroupsStats.kt
+++ src/main/java/com/github/shiverawe/vk/apps/UserGroupsStats.kt
@@ -4,6 +4,19 @@ import com.github.shiverawe.vk.temp.AuthData
 import com.github.shiverawe.vk.temp.Requests
 import com.github.shiverawe.vk.temp.UserIds
 import com.github.shiverawe.vk.util.Utils
+import com.vk.api.sdk.client.actors.UserActor
+import com.vk.api.sdk.exceptions.ApiException
+import com.vk.api.sdk.queries.groups.GroupField
+import java.util.HashSet
+import kotlin.collections.HashMap
+import kotlin.collections.Map
+import kotlin.collections.Set
+import kotlin.collections.filter
+import kotlin.collections.forEach
+import kotlin.collections.getOrPut
+import kotlin.collections.map
+import kotlin.collections.toSet
+import kotlin.collections.toTypedArray
 
 fun main(args: Array<String>) {
     //val code = AuthData.getCode()
@@ -12,8 +25,43 @@ fun main(args: Array<String>) {
 //    val users: IntArray = Requests.getMembersOfGroup(actor, "76477009")
 
 //    val data = Requests.getGroupsOfUsers(actor, *UserIds.asMap.values.toIntArray())
-    val data = Requests.getGroupsOfUsers(actor, *users)
+    val data = getGroupsOfUsers(actor, *users)
 
-    Utils.printMapSorted(data.filter { it.value.size > 1 }, { v1, v2 -> v2.size - v1.size })
+    Utils.printMapSorted(data.filter { it.value.size > 1 }) { v1, v2 -> v2.size - v1.size }
 }
 
+/**
+ * This method
+ * 1. gets all groups of specified users
+ * 2. creates a map, where key is a group name
+ *    and value is list of users from specified users
+ *    which are subscribed to that group
+ */
+fun getGroupsOfUsers(actor: UserActor, vararg userIds: Int): Map<String, Set<String>> {
+    val map = HashMap<String, HashSet<String>>()
+    val userNames: Map<Int, String> = Requests.userName(actor, *userIds.map { it.toString() }.toTypedArray())
+    userIds.forEach { userId ->
+        try {
+            val userGroups: Set<String> = Requests.vk
+                    .groups()
+                    .getExtended(actor)
+                    .userId(userId)
+                    .fields(GroupField.SCREEN_NAME)
+                    .execute()
+                    .items
+                    .map { it.screenName }
+                    .toSet()
+            userGroups.forEach { groupName ->
+                userNames[userId]?.let {
+                    map.getOrPut(groupName) { HashSet() }.add(it)
+                }
+            }
+            Thread.sleep(500)
+        } catch (e: ApiException) {
+            System.err.println("Could not get groups of ${userNames[userId]}")
+        } catch (e: Exception) {
+            System.err.println("Some problem with user ${userNames[userId]}")
+        }
+    }
+    return map
+}
--- src/main/java/com/github/shiverawe/vk/temp/Requests.kt
+++ src/main/java/com/github/shiverawe/vk/temp/Requests.kt
@@ -4,56 +4,14 @@ import com.github.shiverawe.vk.util.Utils
 import com.github.shiverawe.vk.util.Utils.retry
 import com.vk.api.sdk.client.VkApiClient
 import com.vk.api.sdk.client.actors.UserActor
-import com.vk.api.sdk.exceptions.ApiException
 import com.vk.api.sdk.exceptions.ClientException
 import com.vk.api.sdk.httpclient.HttpTransportClient
-import com.vk.api.sdk.queries.groups.GroupField
-import java.lang.System.err
-import java.util.*
-import kotlin.collections.HashMap
 
 object Requests {
 
     val transportClient = HttpTransportClient.getInstance()
 
     val vk = VkApiClient(transportClient)
-
-    /**
-     * This method
-     * 1. gets all groups of specified users
-     * 2. creates a map, where key is a group name
-     *    and value is list of users from specified users
-     *    which are subscribed to that group
-     */
-    fun getGroupsOfUsers(actor: UserActor, vararg userIds: Int): Map<String, Set<String>> {
-        val map = HashMap<String, HashSet<String>>()
-        val userNames: Map<Int, String> = userName(actor, *userIds.map { it.toString() }.toTypedArray())
-        userIds.forEach { userId ->
-            try {
-                val userGroups: Set<String> = vk
-                        .groups()
-                        .getExtended(actor)
-                        .userId(userId)
-                        .fields(GroupField.SCREEN_NAME)
-                        .execute()
-                        .items
-                        .map { it.screenName }
-                        .toSet()
-                userGroups.forEach { groupName ->
-                    userNames[userId]?.let {
-                        map.getOrPut(groupName, { HashSet() }).add(it)
-                    }
-                }
-                Thread.sleep(500)
-            } catch (e: ApiException) {
-                err.println("Could not get groups of ${userNames[userId]}")
-            } catch (e: Exception) {
-                err.println("Some problem with user ${userNames[userId]}")
-            }
-        }
-        return map
-    }
-
 
     fun <K, T> List<T>.mapFor(mapping: T.() -> K): Map<K, T> {
         val map = HashMap<K, T>()
